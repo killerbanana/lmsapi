66ddc05cf1d527d17c92f20578a2baf3d1934b74
killerbanana <josh.rosqueta23@gmail.com>
2025-06-15 18:41:27 +0800

    update roles

--- app/Services/RoleAbilitiesService.php
+++ app/Services/RoleAbilitiesService.php
@@ -9,10 +9,10 @@ class RoleAbilitiesService
     {
         $roleAbilities = [
             'Administrator' => ['*'],
-            'Teacher' => ['score-quiz-submission', 'check-quiz-submissions', 'section-student-linked','send-sms', 'get-announcement',  'post-announcement', 'delete-announcement', 'view-students', 'grade-students', 'update-profile', 'subject-assign-student', 'class-assign-student', 'create-lesson',
+            'Teacher' => ['changePassword', 'score-quiz-submission', 'check-quiz-submissions', 'section-student-linked','send-sms', 'get-announcement',  'post-announcement', 'delete-announcement', 'view-students', 'grade-students', 'update-profile', 'subject-assign-student', 'class-assign-student', 'create-lesson',
         'view-lessons', 'update-announcement', 'create-section-assessment', 'view-classes', 'update-lessons', 'delete-lessons','create-section', 'view-section', 'update-section', 'view-classes-all', 'add-student-lessons', 'update-lesson-progress', 'update-section-progress'],
-            'Student' => ['section-student', 'section-student-linked', 'submit-quiz-answer', 'section-student', 'get-announcement',  'view-grades', 'update-profile', 'answer-module', 'view-classes', 'view-lessons', 'view-section', 'update-section', 'view-classes-all', 'update-lesson-progress', 'update-section-progress'],
-            'Parent' => ['read-only', 'view-students-parent'],
+            'Student' => ['changePassword', 'section-student', 'section-student-linked', 'submit-quiz-answer', 'section-student', 'get-announcement',  'view-grades', 'update-profile', 'answer-module', 'view-classes', 'view-lessons', 'view-section', 'update-section', 'view-classes-all', 'update-lesson-progress', 'update-section-progress'],
+            'Parent' => ['changePassword', 'read-only', 'view-students-parent'],
         ];
 
         // Return the abilities for the given role, or default to 'read-only'
